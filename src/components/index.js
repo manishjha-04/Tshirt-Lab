43563e92a26e5f6fed178fe739ccb40c0e6a7cb4
manishjha-04 <stayconnectedwithmanish@gmail.com>
2023-08-05 00:18:41 +0530

    created buttons for customization part

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,14 @@
+import AIPicker from "./AIPicker";
 import CustomButton from "./CustomButton";
+import ColorPicker from "./ColorPicker";
+import FilePicker from "./FilePicker";
+import Tab from "./Tab";
 
-export {CustomButton};
+
+export {
+    CustomButton,
+    AIPicker,
+    ColorPicker,
+    FilePicker,
+    Tab,
+};
